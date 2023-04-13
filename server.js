13e90fcd8940e8c6b9ff392baeef3ba8ee63b10e
jamessciacca <noing2003@gmail.com>
2023-04-13 19:32:56 -0400

    added paths for the included files, and created the function that allows user to creeate and save a note

--- server.js
+++ server.js
@@ -4,12 +4,55 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 
+//pulling in the unique id module
+const noteId = require('generate-unique-id');
+
 //creating a new instance of the express object
 const app = express();
 
-//creating the route lsitener for the index.html file
-app.get('/', (client_request, server_response)=>{
-    server_response.sendFile(path.join(process.cwd(), './public/index.html'));
+//creating a notesd object to hold the json object
+const {notes} = require('./db/db.json')
+
+//creating middleware instances
+//tells the server that the public file is the root
+app.use(express.static('public'));
+//parses incoming requests with JSON payloads and is based on body-parser
+app.use(express.json());
+//parse the incoming request with urlencoded payloads and is based upon the body-parser
+app.use(express.urlencoded({extended: true}));
+
+//creating the root path that return the homescreen to the user upon visiting the site
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+//creating the /notes path to allow the users to see the notes/html file when the button is clicked
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+//creating the createNote function
+function createNote(body, notesArray){
+    const note = body;
+    //pushing the note to the array
+    notesArray.push(note);
+    //syncing file with new note
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({notes:notesArray}, null, 2));
+    //returning the new note
+    return note;
+}
+
+//creating the /api/notes path to allow the user to see the notes that are saved and added to the json file
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+//creating a post to show/add notes to the screen
+app.post('/api/notes', (req, res) => {
+    req.body.id = noteId();
+    const note = createNote(req.body, notes);
+    //sending the note/s
+    res.json(note);
 });
 
 //creating a listener to know wehen theserver is running
